1ce6bc44c92e952070043045614b2b6a21ca38a7
wk897366661-maker <wk897366661@gmail.com>
2026-04-26 17:54:19 +0800

    更新 MainActivity.java

--- app/src/main/java/com/fengye/vpn/app/src/main/java/com/fengye/vpn/MainActivity.java
+++ app/src/main/java/com/fengye/vpn/app/src/main/java/com/fengye/vpn/MainActivity.java
@@ -2,8 +2,6 @@ package com.fengye.vpn;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.graphics.Color;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -11,24 +9,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setPadding(60, 120, 60, 60);
-        layout.setBackgroundColor(Color.rgb(6, 26, 58));
-
-        TextView title = new TextView(this);
-        title.setText("🍁 枫叶 VPN");
-        title.setTextSize(32);
-        title.setTextColor(Color.WHITE);
-
-        TextView sub = new TextView(this);
-        sub.setText("安全放心");
-        sub.setTextSize(22);
-        sub.setTextColor(Color.rgb(120, 183, 255));
-
-        layout.addView(title);
-        layout.addView(sub);
-
-        setContentView(layout);
+        TextView tv = new TextView(this);
+        tv.setText("这里不要内容");
+        setContentView(tv);
     }
 }
